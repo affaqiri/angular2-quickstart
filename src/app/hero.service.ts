296a0602414d4f0b0ba340523d278de565f2ff36
Ahmad Fraidoon Faqiri <ahmad.faqiri@twido.fr>
2017-05-01 01:30:22 +1000

    step6: routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,9 +3,16 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
+  
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+  
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
   // See the "Take it slow" appendix
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
@@ -13,4 +20,5 @@ export class HeroService {
       setTimeout(() => resolve(this.getHeroes()), 2000);
     });
   }
+  
 }
